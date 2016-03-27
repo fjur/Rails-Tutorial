db87fab753329c3b56849e830af450c1c1156b54
fjur <fjuristovski@gmail.com>
2016-03-27 03:41:36 +0000

    finished step 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
+
+
   def index
     @posts = Post.all
     # @posts = [
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 LighthouseForum::Application.routes.draw do
 
- get 'posts', to: 'posts#index'
-
- get 'posts/:id', to: 'posts#show'
+ get 'posts',     to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  get 'posts/:id', to: 'posts#show'
+  post 'posts',    to: 'posts#create'
 
  # get 'posts/0', to: 'posts#post0'
  # get 'posts/1', to: 'posts#post1'
